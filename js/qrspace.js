846e926929c3fdc564da65c552fde27f773e8d2e
MagicQR <MagicQR@hotmail.com>
2018-05-25 20:37:59 +0800

    v0.206

--- js/qrspace.js
+++ js/qrspace.js
@@ -1,11 +1,11 @@
 jQuery(function($){
 
-$('#btnPrevImage').mousedown(funcion(){
+$('#btnPrevImage').click(funcion(){
 	$('#picPreview').attr('src',"/pics/test/weixincode.jpg");
 	$('#picName').text("/pics/test/weixincode.jpg");
 });
 
-$('#btnNextImage').mousedown(funcion(){
+$('#btnNextImage').click(funcion(){
 	$('#picPreview').attr('src',"/pics/test/weixin-code.png");
 	$('#picName').text("/pics/test/weixin-code.png");
 });
